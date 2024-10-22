839962a8cea57560888c040cc00cacb91e47d161
xCyberpunkx <peeplil6666@gmail.com>
2024-10-22 18:36:07 +0100

    feat timeline

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-
 import Hero from "./components/Hero";
-import Grid from "./components/Grid";
 import { Navbar } from "@/app/components/Navbar";
-
-
+import { Timeline } from "./components/ui/Timeline"; // You may not need this import anymore
+import { TimelineDemo } from "./components/TimelineDemo"; // Import the TimelineDemo component
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <Navbar/>   
+        <Navbar />   
         <Hero />
-        <Grid/>
+        {/* Add the TimelineDemo component here */}
+        <TimelineDemo  />
       </div>
     </main>
   );
 }
+
